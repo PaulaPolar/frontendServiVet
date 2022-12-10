8d13360cef22050b19322157374231a77c7f145d
Ambrusecoding <juanestebancarvallocabrera@gmail.com>
2022-12-10 00:07:40 -0500

    se agrego la funcionalidad de abril modales en agregar y eliminar

--- src/components/Citas.js
+++ src/components/Citas.js
@@ -1,107 +1,256 @@
+import React, { Fragment, useState, useEffect, useContext } from "react";
+import { Dialog, Transition } from "@headlessui/react";
 import "../App.css";
-import { Fragment } from "react";
 import "./Citas.css";
 import eliminar from "../img/delete.png";
 import edit from "../img/edit.png";
 
 const Nosotros = () => {
+  let [isOpen, setIsOpen] = useState(false);
+  let [isOpenEliminar, setIsOpenEliminar] = useState(false);
+  let [isOpenEditar, setIsOpenEditar] = useState(false);
+  
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModalEliminar() {
+    setIsOpenEliminar(false);
+  }
+
+  function openModalEliminar() {
+    setIsOpenEliminar(true);
+  }
+
+
+  function closeModalEditar() {
+    setIsOpen(false);
+  }
+
+  function openModalEditar() {
+    setIsOpen(true);
+  }
+
+
   return (
     <Fragment>
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog
+          as="div"  
+          onClose={closeModal}
+        >
+          <div className="min-h-screen px-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Dialog.Overlay className="fixed inset-0" />
+            </Transition.Child>
+
+            {/* This element is to trick the browser into centering the modal contents. */}
+            <span
+              className="inline-block h-screen align-middle"
+              aria-hidden="true"
+            >
+              &#8203;
+            </span>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <div>
+                <Dialog.Title
+                  as="h3"
+                  className="text-lg font-medium leading-6 text-gray-900"
+                >
+                  <b> Confirmacion </b>
+                </Dialog.Title>
+                <div className="pop-up">
+                  <div className="pop-up-int">
+                    <div className="form-block">
+                      <h1 class="heading-4">Pide tu cita</h1>
+                      <form
+                        id="email-form"
+                        name="email-form"
+                        data-name="Email Form"
+                        method="get"
+                        class="form"
+                        aria-label="Email Form"
+                      >
+                        <div class="campos">
+                          <input
+                            type="text"
+                            class="w-input"
+                            maxlength="256"
+                            name="name"
+                            data-name="Name"
+                            placeholder="Nombre"
+                            id="Nombre"
+                            required
+                          ></input>
+                          <input
+                            type="text"
+                            class="w-input"
+                            maxlength="256"
+                            name="Apellidos"
+                            data-name="Apellidos"
+                            placeholder="Apellidos"
+                            id="Apellidos"
+                            required
+                          ></input>
+                          <input
+                            type="tel"
+                            class="w-input"
+                            maxlength="256"
+                            name="Numero-de-telefono"
+                            data-name="Numero de telefono"
+                            placeholder="Número de telefono"
+                            id="Numero-de-telefono"
+                            required
+                          ></input>
+                          <input
+                            type="email"
+                            class="text-field w-input"
+                            maxlength="256"
+                            name="Email"
+                            data-name="Email"
+                            placeholder="Correo electronico"
+                            id="Email"
+                            required
+                          ></input>
+
+                          <input
+                            type="date"
+                            class="text-field w-input"
+                            id="Date"
+                            required
+                          ></input>
+
+                          <input
+                            type="time"
+                            class="text-field w-input"
+                            id="Time"
+                            required
+                          ></input>
+
+                          <div class="Bpop-up">
+                            <input
+                              type="submit"
+                              value="Aceptar"
+                              data-wait="Please wait..."
+                              class="aceptar w-button"
+                            ></input>
+                            <input
+                              type="submit"
+                              value="Cancelar"
+                              data-wait="Please wait..."
+                              data-w-id="de49a73e-c7bf-c7d2-981d-90315b13d6e7"
+                              class="submit-button w-button"
+                              onClick={closeModal}
+                            ></input>
+                          </div>
+                        </div>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition>
+
+
+      <Transition appear show={isOpenEliminar} as={Fragment}>
+        <Dialog
+          as="div"  
+          onClose={closeModalEliminar}
+        >
+          <div className="min-h-screen px-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Dialog.Overlay className="fixed inset-0" />
+            </Transition.Child>
+
+            {/* This element is to trick the browser into centering the modal contents. */}
+            <span
+              className="inline-block h-screen align-middle"
+              aria-hidden="true"
+            >
+              &#8203;
+            </span>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <div>
+                <Dialog.Title
+                  as="h3"
+                  className="text-lg font-medium leading-6 text-gray-900"
+                >
+                  <b> Eliminar </b>
+                </Dialog.Title>
+                <div className="pop-up2">
+                  <div className="pop-up-int2">
+                    <h1 class="title">Eliminar</h1>
+                    <div class="cpop-up">
+                      <p class="paragraph-2">
+                        ¿Está seguro que quiere eliminar la cita?
+                      </p>
+                    </div>
+                    <div class="cbotones">
+                      <a href="#" class="aceptar w-button  " >
+                        Aceptar
+                      </a>
+                      <a href="#" class="submit-button w-button" onClick={closeModalEliminar}>
+                        Cancelar
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition>
+
+
+
+
       <section className="general">
         <div className="contenedor-citas">
           <div className="arriba">
             <nav className="text-block">mostrar</nav>
-            <a href="#" className="button">
+            <a href="#" className="button" onClick={openModal}>
               + Agregar
             </a>
-            <div className="pop-up">
-              <div className="pop-up-int">
-                <div className="form-block">
-                  <h1 class="heading-4">Pide tu cita</h1>
-                  <form
-                    id="email-form"
-                    name="email-form"
-                    data-name="Email Form"
-                    method="get"
-                    class="form"
-                    aria-label="Email Form"
-                  >
-                    <div class="campos">
-                      <input
-                        type="text"
-                        class="w-input"
-                        maxlength="256"
-                        name="name"
-                        data-name="Name"
-                        placeholder="Nombre"
-                        id="Nombre"
-                        required
-                      ></input>
-                      <input
-                        type="text"
-                        class="w-input"
-                        maxlength="256"
-                        name="Apellidos"
-                        data-name="Apellidos"
-                        placeholder="Apellidos"
-                        id="Apellidos"
-                        required
-                      ></input>
-                      <input
-                        type="tel"
-                        class="w-input"
-                        maxlength="256"
-                        name="Numero-de-telefono"
-                        data-name="Numero de telefono"
-                        placeholder="Número de telefono"
-                        id="Numero-de-telefono"
-                        required
-                      ></input>
-                      <input
-                        type="email"
-                        class="text-field w-input"
-                        maxlength="256"
-                        name="Email"
-                        data-name="Email"
-                        placeholder="Correo electronico"
-                        id="Email"
-                        required
-                      ></input>
-
-                      <input
-                        type="date"
-                        class="text-field w-input"
-                        id="Date"
-                        required
-                      ></input>
-
-                      <input
-                        type="time"
-                        class="text-field w-input"
-                        id="Time"
-                        required
-                      ></input>
-
-                      <div class="Bpop-up">
-                        <input
-                          type="submit"
-                          value="Aceptar"
-                          data-wait="Please wait..."
-                          class="aceptar w-button"
-                        ></input>
-                        <input
-                          type="submit"
-                          value="Cancelar"
-                          data-wait="Please wait..."
-                          data-w-id="de49a73e-c7bf-c7d2-981d-90315b13d6e7"
-                          class="submit-button w-button"
-                        ></input>
-                      </div>
-                    </div>
-                  </form>
-                </div>
-              </div>
-            </div>
           </div>
           <div className="tabla">
             <div className="fila-1">
@@ -127,24 +276,11 @@ const Nosotros = () => {
               <span className="columnas">9:00</span>
               <span className="columnas">Hernan Mendez</span>
               <span className="estadoaceptado">Aceptado</span>
-              <div className="icons">
-              <div className="pop-up2">
-              <div className="pop-up-int2">
-                <h1 class="title">Eliminar</h1>
-                <div class="cpop-up">
-                  <p class="paragraph-2">¿Está seguro que quiere eliminar la cita?</p>
-                </div>
-                <div class="cbotones">
-                  <a href="#" class="aceptar w-button">Aceptar</a>
-                  <a href="#" class="submit-button w-button">Cancelar</a>
-                  </div>
-                
-              </div>
-
-              </div>
-           
+              <div className="icons">     
                 <img className="icon" src={edit} alt="" />
+                <button onClick={openModalEliminar}>
                 <img className="icon" src={eliminar} alt="" />
+                </button>
               </div>
             </div>
 
@@ -155,7 +291,9 @@ const Nosotros = () => {
               <span className="pendiente">Pendientes</span>
               <div className="icons">
                 <img className="icon" src={edit} alt="" />
+                <button onClick={openModalEliminar}>
                 <img className="icon" src={eliminar} alt="" />
+                </button>
               </div>
             </div>
             <div id="grid" className="fila-4">
@@ -164,23 +302,11 @@ const Nosotros = () => {
               <span className="columnas">Hernan Mendez</span>
               <span className="estadoaceptado">Aceptado</span>
               <div className="icons">
-              <div className="pop-up2">
-              <div className="pop-up-int2">
-                <h1 class="title">Eliminar</h1>
-                <div class="cpop-up">
-                  <p class="paragraph-2">¿Está seguro que quiere eliminar la cita?</p>
-                </div>
-                <div class="cbotones">
-                  <a href="#" class="aceptar w-button">Aceptar</a>
-                  <a href="#" class="submit-button w-button">Cancelar</a>
-                  </div>
-                
-              </div>
-
-              </div>
-           
                 <img className="icon" src={edit} alt="" />
+                <button onClick={openModalEliminar}>
                 <img className="icon" src={eliminar} alt="" />
+                </button>
+                
               </div>
             </div>
           </div>
